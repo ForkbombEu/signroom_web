6b5a56676f451f7d17399f4f5b85f6a3d0739cac
Puria Nafisi Azizi <puria@dyne.org>
2023-05-29 12:13:39 +0200

    feat: form snapshots

--- webapp/src/routes/my/(protected)/signatures/+page.svelte
+++ webapp/src/routes/my/(protected)/signatures/+page.svelte
@@ -22,11 +22,12 @@
 
 	$: createModal = false;
 	$: folders = data.folders;
-	const form = superForm(data.form);
-
 	$: filteredItems = folders.filter(
 		(item) => item.name.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1
 	);
+
+	const form = superForm(data.form);
+	export const snapshot = { capture: form.capture, restore: form.restore };
 </script>
 
 <PageTitle title="My Signatures" description="Manage your signatures" />
@@ -34,25 +35,38 @@
 	<Button on:click={() => (createModal = true)} class="w-40 mb-4">Create folder</Button>
 </div>
 <Table hoverable={true}>
-	<TableSearch placeholder="Search by folder name" hoverable={true} bind:inputValue={searchTerm}>
+	<TableSearch
+		placeholder="Search by folder name"
+		hoverable={true}
+		bind:inputValue={searchTerm}
+	>
 		<TableBody>
 			{#each filteredItems as item}
 				<TableBodyRow>
 					<TableBodyCell>
 						<div class="flex items-center">
-							<Folder class="shrink-0 w-20 h-20 mr-2 text-slate-500" />
+							<Folder
+								class="shrink-0 w-20 h-20 mr-2 text-slate-500"
+							/>
 							<div class="flex-col">
-								<Heading tag="h4">{item.name}</Heading>
+								<Heading tag="h4"
+									>{item.name}</Heading
+								>
 								<P>{item.description}</P>
 							</div>
 						</div>
 					</TableBodyCell>
 					<TableBodyCell>
-						<P class="text-slate-500 text-sm">{item.updated}</P></TableBodyCell
+						<P class="text-slate-500 text-sm">{item.updated}</P
+						></TableBodyCell
 					>
 					<TableBodyCell>
-						<Button color="alternative" size="xs"><Pencil class="mr-2 w-4" /> Edit</Button>
-						<Button color="alternative" size="xs"><Share class="mr-2 w-4" /> Share</Button>
+						<Button color="alternative" size="xs"
+							><Pencil class="mr-2 w-4" /> Edit</Button
+						>
+						<Button color="alternative" size="xs"
+							><Share class="mr-2 w-4" /> Share</Button
+						>
 					</TableBodyCell>
 				</TableBodyRow>
 			{/each}
